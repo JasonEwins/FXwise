d3797bc5154094fd0c69bfd779202900b5384434
Jason Ewins <jtradex@gmail.com>
2018-08-08 11:46:25 +0100

    add preloader & fade in currency result

--- js/currency_data.js
+++ js/currency_data.js
@@ -9,23 +9,33 @@ const result = document.querySelector('.calc__result--request');
 const currencyReq = document.querySelector('.calc__result--request-currency');
 const updated = document.querySelector('.calc__result--timestamp');
 const rate = document.querySelector('.calc__result-exchange-rate');
+const answer = document.querySelector('.calc__result');
+const preLoader = document.querySelector('.calc__result-loader');
+
 
 submit.addEventListener('click', async (e) => {
   e.preventDefault();
     try {
+      answer.style.display = 'none';
       getCurrency();
       const total = parseFloat(sum.value);
       response = await fetch(`https://data.fixer.io/api/convert?access_key=05989f30f0f7bcb0f23c3913ea80e662&from=${currencyFrom}&to=${currencyTo}&amount=${total}`);
       data = await response.json();
-      showResult()
+      load();
     } 
     catch (error) {
       console.log(error);
     }
 })
 
+function load() {
+  preLoader.style.display = 'block';
+  const loading = setTimeout(showResult, 1200);
+}
+
 function showResult() {
-  document.querySelector('.calc__result').style.display = 'block';
+  preLoader.style.display = 'none';
+  answer.style.display = 'block';
   rate.textContent = '1' + ' ' + currencyFrom + ' = ' + data.info.rate + ' ' + currencyTo;
   const roundResult = Math.round(data.result * 100) / 100;
   baseAmount.textContent = sum.value + ' ' + currencyFrom + ' =';
